07cc56b7f2a961dda25e62f8e26e9ecc69118fdd
Balaji Patil <balajipatol3252@gmail.com>
2025-11-13 13:35:40 +0530

    update js file

--- vite.config.js
+++ vite.config.js
@@ -36,7 +36,8 @@ export default defineConfig({
         nmmsqz5:"sat_biology_quiz.html",
         nmmsqz6:"sat_geography_quiz.html",
         nmmsqz7:"sat_history_quiz.html",
-        nmmsqz8:"sat_science_quiz.html"
+        nmmsqz8:"sat_science_quiz.html",
+        js:"script.js"
       },
     },
   },
